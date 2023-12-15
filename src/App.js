c8e02f093ae34a76df28992bd5194bfbe51065b3
hftamayo <hftamayo@gmail.com>
2023-12-15 15:30:40 -0600

    including tags for tailwind testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-4xl font-bold">Hello World</h1>
+
+      <p className="text-xl bg-blue-500 text-white p-4 rounded-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        voluptatum.
+      </p>
     </div>
   );
 }
