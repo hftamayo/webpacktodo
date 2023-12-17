9d650320d2ededd064c0237985248a76378ffa6b
hftamayo <hftamayo@gmail.com>
2023-12-17 06:34:47 -0600

    displaying navbar component p1

---
+++ src/Pages/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="w-full h-16 bg-blue-600">
+      <h1 className="text-white text-3x1 font-semibold font-Montserrat">Crud</h1>
+    </div>
+  );
+}
+
+export default Navbar;
