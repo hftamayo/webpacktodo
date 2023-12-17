51cf4a3cdd3ef0b2d47fe3e3f429445387965bd1
hftamayo <hftamayo@gmail.com>
2023-12-17 06:35:08 -0600

    integrating navbar in app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
+import Navbar from "./Pages/Navbar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Hello World</h1>
-
-      <p className="text-xl bg-blue-500 text-white p-6 rounded-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-        voluptatum.
-      </p>
+    <div className="app">
+      <Navbar />
     </div>
   );
 }
