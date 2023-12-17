7424f1207d3116ba18825d24700a250c8b1627e3
hftamayo <hftamayo@gmail.com>
2023-12-16 18:47:00 -0600

    removing app.css tags

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 
-import "./App.css";
-
 function App() {
   return (
     <div>
       <h1 className="text-4xl font-bold">Hello World</h1>
 
-      <p className="text-xl bg-blue-500 text-white p-4 rounded-lg">
+      <p className="text-xl bg-blue-500 text-white p-6 rounded-2xl">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
         voluptatum.
       </p>
