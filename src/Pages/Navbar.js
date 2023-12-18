f7581dcc2814de6a37a134ea39fef6541268030b
hftamayo <hftamayo@gmail.com>
2023-12-18 09:26:38 -0600

    changing text color

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <div className="w-full h-16 bg-blue-600 flex items-center">
-      <h1 className="text-black text-3xl font-semibold">
+      <h1 className="text-white text-3xl font-semibold">
         CRUD Operations
       </h1>
     </div>
