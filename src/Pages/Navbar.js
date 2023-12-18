b57cacdacbd0c7f0b31e38b464c56c4c261086a3
hftamayo <hftamayo@gmail.com>
2023-12-18 09:17:15 -0600

    fixing a typo

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="w-full h-16 bg-blue-600">
-      <h1 className="text-black text-3x1 font-semibold font-Montserrat">CRUD Operations</h1>
+    <div className="w-full h-16 bg-blue-600 flex items-center">
+      <h1 className="text-black text-3xl font-semibold">
+        CRUD Operations
+      </h1>
     </div>
   );
 }
