3427ffd6dabb2f1ebce0252f208c643f9c8c40ef
hftamayo <hftamayo@gmail.com>
2023-12-20 11:45:51 -0600

    navigation to crud view function

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const showStudents = () => {
+    navigate("/students");
+  };
+
   return (
     <div className="w-full h-full flex flex-col px-10 py-8">
-      <h1 className="text-black text-3xl font-semibold font-Montserrat">Home</h1>
+      <button
+        className="w-48 bg-black text-blue-400 font-semibold text-xl h-12 rounded-lg"
+        onClick={showStudents}
+      >
+        Students
+      </button>
     </div>
   );
 }
