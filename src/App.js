497462adfc05b3137fc854e3310807d412177beb
hftamayo <hftamayo@gmail.com>
2023-12-21 13:18:56 -0600

    adding the view student route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./Pages/Navbar";
 import Dashboard from "./Pages/Dashboard";
 import { Routes, Route } from "react-router-dom";
 import ViewStudents from "./components/entities/students/ViewStudents";
+import EditStudent from "./components/entities/students/EditStudent";
+import DetailStudent from "./components/entities/students/DetailStudent";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/students" element={<ViewStudents />} />        
+        <Route path="/student/:id" element={<DetailStudent />} />                
       </Routes>
     </div>
   );
