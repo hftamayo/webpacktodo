ebbe600d3ad562f9af8e4327067f7996e2e98f1b
hftamayo <hftamayo@gmail.com>
2023-12-21 12:14:00 -0600

    iterating data

--- src/components/entities/students/DisplayDataTable.js
+++ src/components/entities/students/DisplayDataTable.js
@@ -1,28 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function DisplayDataTable() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3004/students").then((response) => {
+      setUsers(response.data);
+    });
+  }, [users]);
+
   return (
-    <div class="w-full flex flex-col min-h-[50vh] justify-center items-center">
+    <div className="w-full flex flex-col min-h-[50vh] justify-center items-center">
       <h1 className="text-black text-3xl font-semibold font-Montserrat">
         Students Catalog
       </h1>
-      <table class="w-[80%] text-center overflow-hidden overflow-y-scroll mt-8">
-        <thead class="border-b bg-gray-800">
+      <table className="w-[80%] text-center overflow-hidden overflow-y-scroll mt-8 border-2 border-b-2 border-black">
+        <thead className="border-b bg-gray-800">
           <tr>
-            <th scope="col" class="text-sm font-medium text-white px-6 py-4">
+            <th
+              scope="col"
+              className="text-sm font-medium text-white px-6 py-4"
+            >
               ID
             </th>
-            <th class="text-sm font-medium text-white px-6 py-4">Name</th>
-            <th class="text-sm font-medium text-white px-6 py-4">Email</th>
-            <th class="text-sm font-medium text-white px-6 py-4">Phone</th>
-            <th class="text-sm font-medium text-white px-6 py-4">Address</th>
-            <th class="text-sm font-medium text-white px-6 py-4">Actions</th>
+            <th className="text-sm font-medium text-white px-6 py-4">Name</th>
+            <th className="text-sm font-medium text-white px-6 py-4">Email</th>
+            <th className="text-sm font-medium text-white px-6 py-4">Phone</th>
+            <th className="text-sm font-medium text-white px-6 py-4">
+              Address
+            </th>
+            <th className="text-sm font-medium text-white px-6 py-4">
+              Actions
+            </th>
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b">
-            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"></td>
-          </tr>
+          {users.map((data, index) => (
+            <tr key={index} className="bg-white border-b">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                {index + 1}
+              </td>
+              <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {data.name}
+              </td>
+              <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {data.email}
+              </td>
+              <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {data.phoneNumber}
+              </td>
+              <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {data.address.city}
+              </td>
+              <td className="flex justify-center items-center space-x-4 mt-1">
+                <button className="px-6 py-2 font-normal text-white bg-black rounded-lg">
+                  View
+                </button>
+                <button className="px-6 py-2 font-normal text-white bg-blue-600 rounded-lg">
+                  Edit
+                </button>
+                <button className="px-6 py-2 font-normal text-white bg-red-600 rounded-lg">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
