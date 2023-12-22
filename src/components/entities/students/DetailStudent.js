d260e071aa50b6ee7e6dfde8d1d90b506631350b
hftamayo <hftamayo@gmail.com>
2023-12-22 10:27:57 -0600

    pre-alpha of detail student

--- src/components/entities/students/DetailStudent.js
+++ src/components/entities/students/DetailStudent.js
@@ -12,9 +12,65 @@ function DetailStudent() {
   }, [user]);
 
   const { id } = useParams();
-  return <div>{user && (
-    <h1>{user.name}</h1>
-  )}</div>;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-[750px] h-[425px] flex px-6 py-4 border border-black mt-16">
+        <div className="w-5/12 flex flex-col space-y-4">
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Name
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Email
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Phone Number
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Street
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            City
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Zip Code
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Company
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Address
+          </h2>
+        </div>
+
+        <div className="w-7/12 flex flex-col space-y-4">
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.name}
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.email}
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.phoneNumber}
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.address.street}
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.address.city}
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.address.zipcode}
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.company.name}
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.company.address}
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DetailStudent;
