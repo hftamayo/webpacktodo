de4992b1bcc953c7df8fc3c23b29ccb567c1b0c3
hftamayo <hftamayo@gmail.com>
2023-12-22 11:00:38 -0600

    new student button

--- src/components/entities/students/ViewStudents.js
+++ src/components/entities/students/ViewStudents.js
@@ -1,9 +1,19 @@
 import React from "react";
 import DisplayDataTable from "./DisplayDataTable";
+import { Link } from "react-router-dom";
 
 function ViewStudents() {
   return (
     <div className="w-full h-full flex flex-col px-10 py-8">
+      <div>
+        <Link
+          to={`/newstudent`}
+          className="px-6 py-2 font-normal text-white bg-green-600 rounded-lg"
+        >
+          Add
+        </Link>
+      </div>
+
       <DisplayDataTable />
     </div>
   );
