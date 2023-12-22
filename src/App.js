1801ab994f243e0b368783bc96552aa06df0e64e
hftamayo <hftamayo@gmail.com>
2023-12-22 10:58:42 -0600

    newstudent route path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import ViewStudents from "./components/entities/students/ViewStudents";
 import EditStudent from "./components/entities/students/EditStudent";
 import DetailStudent from "./components/entities/students/DetailStudent";
+import AddStudent from "./components/entities/students/AddStudent";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/students" element={<ViewStudents />} />        
-        <Route path="/student/:id" element={<DetailStudent />} />                
+        <Route path="/student/:id" element={<DetailStudent />} />     
+        <Route path="/newstudent/" element={<AddStudent />} />"          
       </Routes>
     </div>
   );
