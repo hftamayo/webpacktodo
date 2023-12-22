62fe4253253ab8ea1fac1996e94b8fbb4b513aff
hftamayo <hftamayo@gmail.com>
2023-12-22 10:34:02 -0600

    adding a link to root

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="w-full h-16 bg-blue-600 flex items-center px-10 py-2 justify-between">
+      <Link to="/">
       <h1 className="text-white text-3xl font-semibold font-Montserrat">Dashboard</h1>
+      </Link>
       <button className="w-48 bg-white text-blue-400 font-semibold text-xl h-12 rounded-lg">
         User
       </button>
