e72213de98e8808455486546ad8a1a044c2e1725
hftamayo <hftamayo@gmail.com>
2023-12-22 10:53:23 -0600

    rearranging go back link

--- src/components/entities/students/DetailStudent.js
+++ src/components/entities/students/DetailStudent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function DetailStudent() {
   const [user, setUser] = useState();
@@ -13,7 +14,13 @@ function DetailStudent() {
 
   const { id } = useParams();
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div>
+        <h1 className="text-blue-500 text-3xl font-semibold font-Montserrat">
+          <Link to="/students">Go back</Link>
+        </h1>
+      </div>
+
       <div className="w-[750px] h-[425px] flex px-6 py-4 border border-black mt-16">
         <div className="w-5/12 flex flex-col space-y-4">
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
