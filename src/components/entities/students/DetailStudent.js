7313b13c25b7230a30b590581d62ac0e9d1144b0
hftamayo <hftamayo@gmail.com>
2023-12-25 17:36:04 -0600

    including all model properties

--- src/components/entities/students/DetailStudent.js
+++ src/components/entities/students/DetailStudent.js
@@ -23,18 +23,24 @@ function DetailStudent() {
 
       <div className="w-[750px] h-[425px] flex justify-center items-center px-6 py-4 border border-black mt-16">
         <div className="w-5/12 flex flex-col space-y-4">
+        <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Badge
+          </h2>          
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Name
+            Full Name
           </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            User Name
+          </h2>          
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             Email
           </h2>
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Phone Number
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             Street
           </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Suite
+          </h2>          
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             City
           </h2>
@@ -42,7 +48,16 @@ function DetailStudent() {
             Zip Code
           </h2>
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Company
+            X
+          </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Y
+          </h2>                    
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Phone Number
+          </h2>          
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            Company's Name
           </h2>
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             Address
@@ -50,24 +65,39 @@ function DetailStudent() {
         </div>
 
         <div className="w-7/12 flex flex-col space-y-4">
+        <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.id}
+          </h2>          
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             {user?.name}
           </h2>
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {user?.email}
-          </h2>
+            {user?.username}
+          </h2>          
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {user?.phoneNumber}
+            {user?.email}
           </h2>
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             {user?.address.street}
           </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.address.suite}
+          </h2>          
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             {user?.address.city}
           </h2>
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             {user?.address.zipcode}
           </h2>
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.position.lat}
+          </h2>          
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.position.lng}
+          </h2>          
+          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
+            {user?.phoneNumber}
+          </h2>          
           <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
             {user?.company.name}
           </h2>
