f2006d3cd87666bd1d015b10779a5933ab745ad9
hftamayo <hftamayo@gmail.com>
2023-12-27 13:09:15 -0600

    adding more properties part2

--- src/components/entities/students/AddStudent.jsx
+++ src/components/entities/students/AddStudent.jsx
@@ -67,7 +67,7 @@ function AddStudent() {
 
         <input
           value={email}
-          onChange={(e) => setStreet(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
           type="email"
           placeholder="Email"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
@@ -75,19 +75,76 @@ function AddStudent() {
 
         <input
           value={street}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setStreet(e.target.value)}
           type="text"
           placeholder="Street"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
 
         <input
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+          value={suite}
+          onChange={(e) => setSuite(e.target.value)}
+          type="text"
+          placeholder="Suite/Apt"
+          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+        />
+
+        <input
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          type="text"
+          placeholder="City"
+          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+        />
+
+        <input
+          value={zipcode}
+          onChange={(e) => setZipcode(e.target.value)}
+          type="text"
+          placeholder="Zipcode"
+          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+        />
+
+        <input
+          value={latitude}
+          onChange={(e) => setLatitude(e.target.value)}
+          type="text"
+          placeholder="Latitude"
+          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+        />
+
+        <input
+          value={longitude}
+          onChange={(e) => setLongitude(e.target.value)}
+          type="text"
+          placeholder="Longitude"
+          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+        />
+
+        <input
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
           type="phone"
           placeholder="Phone Number"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
+
+        <input
+          value={companyName}
+          onChange={(e) => setCompanyName(e.target.value)}
+          type="text"
+          placeholder="Company's name"
+          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+        />
+
+        <input
+          value={companyAddress}
+          onChange={(e) => setCompanyAddress(e.target.value)}
+          type="text"
+          placeholder="Company's address"
+          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+        />
+
         <div className="w-screen h-full flex justify-center items-center space-x-4 mt-16">
           <button
             onClick={submit}
