4e31009c69b7269c68ad415f1f8c885c73514bc1
hftamayo <hftamayo@gmail.com>
2023-12-31 18:23:49 -0600

    adding labels to each property for better comprehension

--- src/components/entities/students/EditStudent.jsx
+++ src/components/entities/students/EditStudent.jsx
@@ -77,46 +77,93 @@ function EditStudent() {
       </h1>
 
       <form className="w-[80%] h-full flex flex-col justify-center items-center mt-4">
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-          placeholder="Name"
-          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
-        />
+        <div className="flex items-center space-x-4">
+          <label
+            htmlFor="name"
+            className="text-black font-semibold font-Inter text-xl"
+          >
+            Full Name
+          </label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Name"
+            className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+          />
+        </div>
 
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          type="text"
-          placeholder="User name"
-          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
-        />
+        <div className="flex items-center space-x-4">
+          <label
+            htmlFor="username"
+            className="text-black font-semibold font-Inter text-xl"
+          >
+            User Name
+          </label>
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            placeholder="User name"
+            className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+          />
+        </div>
 
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="Email"
-          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
-        />
+        <div className="flex items-center space-x-4">
+          <label
+            htmlFor="email"
+            className="text-black font-semibold font-Inter text-xl"
+          >
+            Email
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email"
+            className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+          />
+        </div>
 
-        <input
-          value={street}
-          onChange={(e) => setStreet(e.target.value)}
-          type="text"
-          placeholder="Street"
-          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
-        />
+        <div className="flex items-center space-x-4">
+          <label
+            htmlFor="street"
+            className="text-black font-semibold font-Inter text-xl"
+          >
+            Street
+          </label>
+          <input
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
+            type="text"
+            placeholder="Street"
+            className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+          />
+        </div>
 
-        <input
-          value={suite}
-          onChange={(e) => setSuite(e.target.value)}
-          type="text"
-          placeholder="Suite/Apt"
-          className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
-        />
+        <div className="flex items-center space-x-4">
+          <label
+            htmlFor="suite"
+            className="text-black font-semibold font-Inter text-xl"
+          >
+            Suite/Apartment
+          </label>
+          <input
+            value={suite}
+            onChange={(e) => setSuite(e.target.value)}
+            type="text"
+            placeholder="Suite/Apt"
+            className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
+          />
+        </div>
 
+        <div className="flex items-center space-x-4">        
+        <label
+          htmlFor="city"
+          className="text-black font-semibold font-Inter text-xl"
+        >
+          City
+        </label>
         <input
           value={city}
           onChange={(e) => setCity(e.target.value)}
@@ -124,7 +171,15 @@ function EditStudent() {
           placeholder="City"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
+        </div>
 
+        <div className="flex items-center space-x-4">        
+        <label
+          htmlFor="zipcode"
+          className="text-black font-semibold font-Inter text-xl"
+        >
+          ZIP Code
+        </label>
         <input
           value={zipcode}
           onChange={(e) => setZipcode(e.target.value)}
@@ -132,7 +187,15 @@ function EditStudent() {
           placeholder="Zipcode"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
+        </div>
 
+        <div className="flex items-center space-x-4">        
+        <label
+          htmlFor="latitude"
+          className="text-black font-semibold font-Inter text-xl"
+        >
+          Latitude
+        </label>
         <input
           value={latitude}
           onChange={(e) => setLatitude(e.target.value)}
@@ -140,7 +203,15 @@ function EditStudent() {
           placeholder="Latitude"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
+        </div>
 
+        <div className="flex items-center space-x-4">        
+        <label
+          htmlFor="longitude"
+          className="text-black font-semibold font-Inter text-xl"
+        >
+          Longitude
+        </label>
         <input
           value={longitude}
           onChange={(e) => setLongitude(e.target.value)}
@@ -148,7 +219,15 @@ function EditStudent() {
           placeholder="Longitude"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
+        </div>
 
+        <div className="flex items-center space-x-4">        
+        <label
+          htmlFor="phoneNumber"
+          className="text-black font-semibold font-Inter text-xl"
+        >
+          Phone Number
+        </label>
         <input
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
@@ -156,7 +235,15 @@ function EditStudent() {
           placeholder="Phone Number"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
+        </div>
 
+        <div className="flex items-center space-x-4">        
+        <label
+          htmlFor="companyName"
+          className="text-black font-semibold font-Inter text-xl"
+        >
+          Company's Name
+        </label>
         <input
           value={companyName}
           onChange={(e) => setCompanyName(e.target.value)}
@@ -164,7 +251,15 @@ function EditStudent() {
           placeholder="Company's name"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
+        </div>
 
+        <div className="flex items-center space-x-4">        
+        <label
+          htmlFor="companyAddress"
+          className="text-black font-semibold font-Inter text-xl"
+        >
+          Comapny's Address
+        </label>
         <input
           value={companyAddress}
           onChange={(e) => setCompanyAddress(e.target.value)}
@@ -172,6 +267,7 @@ function EditStudent() {
           placeholder="Company's address"
           className="w-[80%] bg-white/10 text-xl mt-4 font-Montserrat font-normal outline-none py-4 pl-6 border border-zinc-400"
         />
+        </div>
 
         <div className="w-screen h-full flex justify-center items-center space-x-4 mt-16">
           <button
