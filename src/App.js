90c020e2ad71ebb7d4988e110aa2b2444b36a68a
hftamayo <hftamayo@gmail.com>
2024-01-04 07:34:28 -0600

    rearranging browserrouter tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./Pages/Navbar";
 import Dashboard from "./Pages/Dashboard";
 import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ViewStudents from "./components/entities/students/ViewStudents";
 import EditStudent from "./components/entities/students/EditStudent";
 import DetailStudent from "./components/entities/students/DetailStudent";
@@ -10,6 +11,7 @@ import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <div className="app">
+      <BrowserRouter >
       <Navbar />
       <ToastContainer position="bottom-left" />
       <Routes>
@@ -19,6 +21,7 @@ function App() {
         <Route path="/newstudent/" element={<AddStudent />} />"
         <Route path="/editstudent/:id" element={<EditStudent />} />
       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import "tailwindcss/tailwind.css";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import store from "./components/store/store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
