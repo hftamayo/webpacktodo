6de59d3d7c77d1d40e98921d1cd463af50d19c74
hftamayo <hftamayo@gmail.com>
2024-01-05 07:46:13 -0600

    improving import classes

--- src/components/entities/students/AddStudent.jsx
+++ src/components/entities/students/AddStudent.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import { toast } from "react-toastify";
 import { addStudent } from "../../store/studentSlice";
 import { useDispatch } from "react-redux";
