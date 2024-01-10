d5fa8d88cb446368ad229ca3dc961663ba7615a9
hftamayo <hftamayo@gmail.com>
2024-01-10 09:25:42 -0600

    passing props to EntityRow

--- src/components/entities/students/DisplayDataTable.js
+++ src/components/entities/students/DisplayDataTable.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getStudents, deleteStudent } from "../../store/studentSlice";
 import { toast } from "react-toastify";
-import { FaTrash, FaEye, FaEdit } from "react-icons/fa";
-import ConfirmDialogBox from "../../ui/ConfirmDialogBox";
+
+import EntityRow from "./EntityRow";
 
 function DisplayDataTable() {
   const dispatch = useDispatch();
@@ -13,10 +12,7 @@ function DisplayDataTable() {
   const [selectedStudentName, setSelectedStudentName] = useState(null);
   const [confirmDialogBoxOpen, setConfirmDialogBoxOpen] = useState(false);
 
-  const ENTITY_ROW_CLASSNAME = "bg-white border-b";
   const ENTITY_HEADER_CLASSNAME = "text-sm font-medium text-white px-6 py-4";
-  const ENTITY_CELL_CLASSNAME =
-    "text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap";
 
   const loadStudents = () => {
     try {
@@ -83,47 +79,6 @@ function DisplayDataTable() {
     setConfirmDialogBoxOpen(false);
   };
 
-  const EntityRow = ({entity}) => (
-    <tr className={ENTITY_ROW_CLASSNAME}>
-      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-        {entity.id}
-      </td>
-      <td className={ENTITY_CELL_CLASSNAME}>{entity.name}</td>
-      <td className={ENTITY_CELL_CLASSNAME}>{entity.email}</td>
-      <td className={ENTITY_CELL_CLASSNAME}>{entity.phoneNumber}</td>
-      <td className={ENTITY_CELL_CLASSNAME}>{entity.address.city}</td>
-      <td className="flex justify-center items-center space-x-4 mt-1">
-        <Link
-          to={`/student/${entity.id}`}
-          className="flex items-center px-6 py-2 font-normal text-white bg-black rounded-lg w-30"
-        >
-          <FaEye />
-          <span className="ml-2">View</span>
-        </Link>
-        <Link
-          to={`/editstudent/${entity.id}`}
-          className="flex items-center px-6 py-2 font-normal text-white bg-blue-600 rounded-lg w-30"
-        >
-          <FaEdit />
-          <span className="ml-2">Edit</span>
-        </Link>
-        <Link
-          onClick={() => handleOpenConfirmDialogBox(entity.id, entity.name)}
-          className="flex items-center px-6 py-2 font-normal text-white bg-red-600 rounded-lg w-30"
-        >
-          <FaTrash />
-          <span className="ml-2">Delete</span>
-        </Link>
-        <ConfirmDialogBox
-          isOpen={confirmDialogBoxOpen}
-          onClose={handleCloseConfirmDialogBox}
-          selectedEntityName={selectedStudentName}
-          onDelete={handleDeleteStudent}
-        />
-      </td>
-    </tr>
-  );
-
   return (
     <div className="w-full flex flex-col min-h-[50vh] justify-center items-center">
       <h1 className="text-black text-3xl font-semibold font-Montserrat">
@@ -144,7 +99,15 @@ function DisplayDataTable() {
         </thead>
         <tbody>
           {students.map((data) => (
-            <EntityRow key={data.id} entity={data} />
+            <EntityRow
+              key={data.id}
+              entity={data}
+              selectedStudentName={selectedStudentName}
+              confirmDialogBoxOpen={confirmDialogBoxOpen}
+              handleDeleteStudent={handleDeleteStudent}
+              handleOpenConfirmDialogBox={handleOpenConfirmDialogBox}
+              handleCloseConfirmDialogBox={handleCloseConfirmDialogBox}
+            />
           ))}
         </tbody>
       </table>
