461391326611b6bef4d7a2507a1677eca6ce1784
hftamayo <hftamayo@gmail.com>
2024-01-12 09:19:29 -0600

    defining the way how to show the data

--- src/components/entities/students/DetailStudent.js
+++ src/components/entities/students/DetailStudent.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getStudent } from "../../store/studentSlice";
 import { toast } from "react-toastify";
 
 function DetailStudent() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const studentId = Number.parseInt(id);
   const dispatch = useDispatch();
   const student = useSelector((state) =>
@@ -35,98 +36,36 @@ function DetailStudent() {
   }
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <div>
-        <h1 className="text-blue-500 text-3xl font-semibold font-Montserrat mt-8">
-          <Link to="/students">Go back</Link>
-        </h1>
-      </div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <form className="flex flex-col items-center justify-center w-full max-w-2xl px-4 py-8 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-6">Current Student Information</h2>
 
-      <div className="w-[750px] h-[425px] flex justify-center items-center px-6 py-4 border border-black mt-16">
-        <div className="w-5/12 flex flex-col space-y-4">
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Badge
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Full Name
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            User Name
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Email
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Street
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Suite
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            City
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Zip Code
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            X
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Y
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Phone Number
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Company's Name
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            Address
-          </h2>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-1 flex items-center justify-center">
+            <p className="block text-md font-medium text-gray-700">Full Name:</p>
+          </div>
+          <div className="col-span-2">
+            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
+              {student?.name}
+            </p>
+          </div>
         </div>
 
-        <div className="w-7/12 flex flex-col space-y-4">
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.id}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.name}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.username}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.email}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.address.street}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.address.suite}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.address.city}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.address.zipcode}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.position.lat}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.position.lng}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.phoneNumber}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.company.name}
-          </h2>
-          <h2 className="text-black font-semibold font-Inter text-2xl border-b border-black">
-            {student?.company.address}
-          </h2>
+        <div className="space-x-4 mt-6">
+          <button
+            onClick={() => navigate(`/students/${studentId}/edit`)}
+            className="px-4 py-2 text-base font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => navigate("/students")}
+            className="px-4 py-2 text-base font-medium text-white bg-yellow-600 rounded-md hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+          >
+            Go Back
+          </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
