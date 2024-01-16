9309c55c942b2a02d6a894e8871722869ddd8fef
hftamayo <hftamayo@gmail.com>
2024-01-16 09:20:59 -0600

    fixing a typo

--- src/components/entities/students/AddStudent.jsx
+++ src/components/entities/students/AddStudent.jsx
@@ -406,7 +406,7 @@ function AddStudent() {
           onClick={handleSubmit}
           className="px-4 py-2 text-base font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          Update
+          Save
         </button>
         <button
           onClick={() => navigate("/students")}
