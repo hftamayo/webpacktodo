493772c9ead17c54ea5617eccb4b0a5ecf4e52da
hftamayo <hftamayo@gmail.com>
2024-03-06 14:41:17 -0600

    rearranging detail student

--- src/components/entities/students/DetailStudent.js
+++ src/components/entities/students/DetailStudent.js
@@ -36,145 +36,192 @@ function DetailStudent() {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <form className="flex flex-col items-center justify-center w-full max-w-2xl px-4 py-8 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-6">Current Student Information</h2>
-
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">
-              Full Name:
-            </p>
+    <div className="flex justify-center">
+      <form className="w-full max-w-lg">
+        <div className="rounded overflow-hidden shadow-lg p-6 bg-white">
+          <div className="flex items-center justify-center font-bold text-xl mb-2">
+            Student Information
           </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.name}
-            </p>
+          <div>
+            <div className="flex flex-wrap -mx-3">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  Full Name
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.name ? (
+                    student.name
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  User Name
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.username ? (
+                    student.username
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">Email</p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.email ? (
+                    student.email
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  Street
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.address.street ? (
+                    student.address.street
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">Suite</p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.address.suite ? (
+                    student.address.suite
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">City</p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.address.city ? (
+                    student.address.city
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  Zipcode
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.address.zipcode ? (
+                    student.address.zipcode
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  Latitude
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.position.lat ? (
+                    student.position.lat
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  Longitude
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.position.lng ? (
+                    student.position.lng
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  Phone Number
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.phoneNumber ? (
+                    student.phoneNumber
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  Company Name
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.company.companyName ? (
+                    student.company.companyName
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <p className="block text-md font-medium text-gray-700">
+                  Company Address
+                </p>
+                <p
+                  className={`appearance-none block w-full bg-gray-200 text-gray-700 font-bold border rounded py-3 px-4 mb-3 leading-tight }`}
+                >
+                  {student?.company.companyAddress ? (
+                    student.company.companyAddress
+                  ) : (
+                    <span className="text-red-500">N/A</span>
+                  )}
+                </p>
+              </div>
+            </div>
           </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">
-              User Name:
-            </p>
+          <div className="flex justify-center mt-4 px-4 space-x-4">
+            <button
+              onClick={() => navigate(`/editstudent/${studentId}`)}
+              className="px-4 py-2 text-base font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => navigate("/students")}
+              className="px-4 py-2 text-base font-medium text-white bg-yellow-600 rounded-md hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+            >
+              Cancel
+            </button>
           </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.username}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">Email:</p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.email}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">Street:</p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.address.street}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">Suite:</p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.address.suite}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">City:</p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.address.city}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">Zip Code:</p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.address.zipcode}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">Latitude:</p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.position.lat}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">
-              Longitude:
-            </p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.position.lng}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">
-              Phone Number:
-            </p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.phoneNumber}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">
-              Company Name:
-            </p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.company.companyName}
-            </p>
-          </div>
-
-          <div className="col-span-1 flex items-center justify-center">
-            <p className="block text-md font-medium text-gray-700">
-              Company Address:
-            </p>
-          </div>
-          <div className="col-span-2">
-            <p className="w-[100%] text-lg font-bold px-3 py-2 ">
-              {student?.company.companyAddress}
-            </p>
-          </div>
-        </div>
-
-        <div className="space-x-4 mt-6">
-          <button
-            onClick={() => navigate(`/editstudent/${studentId}`)}
-            className="px-4 py-2 text-base font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => navigate("/students")}
-            className="px-4 py-2 text-base font-medium text-white bg-yellow-600 rounded-md hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-          >
-            Go Back
-          </button>
         </div>
       </form>
     </div>
