70b7e20940cbd32fbfdb98f0664c8b0897fb3390
hftamayo <hftamayo@gmail.com>
2024-03-15 08:11:40 -0600

    improving code imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from "./Pages/Navbar";
 import Dashboard from "./Pages/Dashboard";
-import { Routes, Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ViewStudents from "./components/entities/students/ViewStudents";
 import EditStudent from "./components/entities/students/EditStudent";
 import DetailStudent from "./components/entities/students/DetailStudent";
@@ -11,16 +10,16 @@ import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <div className="app">
-      <BrowserRouter >
-      <Navbar />
-      <ToastContainer position="bottom-left" />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/students" element={<ViewStudents />} />
-        <Route path="/student/:id" element={<DetailStudent />} />
-        <Route path="/newstudent/" element={<AddStudent />} />"
-        <Route path="/editstudent/:id" element={<EditStudent />} />
-      </Routes>
+      <BrowserRouter>
+        <Navbar />
+        <ToastContainer position="bottom-left" />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/students" element={<ViewStudents />} />
+          <Route path="/student/:id" element={<DetailStudent />} />
+          <Route path="/newstudent/" element={<AddStudent />} />"
+          <Route path="/editstudent/:id" element={<EditStudent />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
