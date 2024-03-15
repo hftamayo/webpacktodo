8f757dcff6db315df7379c5b807a7b273a11a415
hftamayo <hftamayo@gmail.com>
2024-03-15 08:55:34 -0600

    initialState and reportSlicer objects

--- src/components/store/reportSlice.js
+++ src/components/store/reportSlice.js
@@ -16,3 +16,45 @@ export const xlsReport = createAsyncThunk(
     return response.data;
   }
 );
+
+const initialState = {
+  report: [],
+  status: "idle",
+  error: null,
+};
+
+const reportSlice = createSlice({
+  name: "report",
+  initialState,
+  reducers: {
+    reportAdded(state, action) {
+      state.report.push(action.payload);
+    },
+  },
+  extraReducers: {
+    [pdfReport.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [pdfReport.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state.report = action.payload;
+    },
+    [pdfReport.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message;
+    },
+    [xlsReport.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [xlsReport.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state.report = action.payload;
+    },
+    [xlsReport.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message;
+    },
+  },
+});
+
+export default reportSlice.reducer;
