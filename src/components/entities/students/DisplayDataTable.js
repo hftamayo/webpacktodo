babecbf8d3c2e24a7bdf133476de62286cb16517
hftamayo <hftamayo@gmail.com>
2024-03-28 07:25:22 -0600

    decoupling navigation part1

--- src/components/entities/students/DisplayDataTable.js
+++ src/components/entities/students/DisplayDataTable.js
@@ -3,10 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { getStudents, deleteStudent } from "../../store/studentSlice";
 import { toast } from "react-toastify";
 import {
-  FaAngleLeft,
-  FaAngleRight,
-  FaAngleDoubleLeft,
-  FaAngleDoubleRight,
   FaSort,
   FaFilePdf,
   FaFileExcel,
@@ -14,6 +10,7 @@ import {
 
 import EntityRow from "./EntityRow";
 import dataTableClasses from "../../ui/crud/dataTableclasses";
+import PaginationBar from "../../ui/crud/NavigationBar";
 
 function DisplayDataTable() {
   const dispatch = useDispatch();
@@ -34,12 +31,6 @@ function DisplayDataTable() {
   const [sortDirection, setSortDirection] = useState("asc");
 
   const [confirmDialogBoxOpen, setConfirmDialogBoxOpen] = useState(false);
-
-  const NAVIGATION_BUTTON_CLASSNAME = `${dataTableClasses.navButton} ${
-    students.length === 0 || currentPage === 1 || recordsPerPage === -1
-      ? "cursor-not-allowed"
-      : ""
-  }`;
 
   const loadData = useCallback(() => {
     try {
@@ -310,80 +301,15 @@ function DisplayDataTable() {
           )}
         </tbody>
       </table>
-      <nav className="flex items-center justify-between pt-4">
-        <div className="flex items-center justify-center">
-          <button
-            className={NAVIGATION_BUTTON_CLASSNAME}
-            disabled={
-              students.length === 0 ||
-              currentPage === 1 ||
-              recordsPerPage === -1
-            }
-            onClick={() => setCurrentPage(1)}
-          >
-            <FaAngleDoubleLeft className="h-5 w-5" />
-          </button>
-          <div className="border-t border-gray-200 my-4 px-1"></div>
-          <button
-            className={NAVIGATION_BUTTON_CLASSNAME}
-            disabled={
-              students.length === 0 ||
-              currentPage === 1 ||
-              recordsPerPage === -1
-            }
-            onClick={() => setCurrentPage((old) => Math.max(old - 1, 1))}
-          >
-            <FaAngleLeft className="h-5 w-5" />
-          </button>
-          <div className="border-gray-200 my-4 px-1"></div>
-          <button
-            className={NAVIGATION_BUTTON_CLASSNAME}
-            disabled={
-              students.length === 0 ||
-              currentPage === totalPages ||
-              recordsPerPage === -1
-            }
-            onClick={() =>
-              setCurrentPage((old) => Math.min(old + 1, totalPages))
-            }
-          >
-            <FaAngleRight className="h-5 w-5" />
-          </button>
-          <div className="border-t border-gray-200 my-4 px-1"></div>
-          <button
-            className={NAVIGATION_BUTTON_CLASSNAME}
-            disabled={
-              students.length === 0 ||
-              currentPage === totalPages ||
-              recordsPerPage === -1
-            }
-            onClick={() => setCurrentPage(totalPages)}
-          >
-            <FaAngleDoubleRight className="h-5 w-5" />
-          </button>
-        </div>
-        <div className="flex items-center">
-          <div className="border-gray-200 my-4 px-1"></div>
-          Go To Page:
-          <div className="border-gray-200 my-4 px-1"></div>
-          <select
-            value={currentPage}
-            onChange={(e) => setCurrentPage(Number(e.target.value))}
-            className="px-3 py-0 bg-gray-800 text-white rounded-md"
-          >
-            {pageNumbers.map((number) => (
-              <option
-                key={number}
-                value={number}
-                disabled={recordsPerPage === -1}
-              >
-                {number}
-              </option>
-            ))}
-          </select>
-          <div className="border-gray-200 my-4 px-1"></div>
-        </div>
-      </nav>
+      <PaginationBar
+      students={students}
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+      recordsPerPage={recordsPerPage}
+      totalPages={totalPages}
+      pageNumbers={pageNumbers}
+       />
+
     </div>
   );
 }
