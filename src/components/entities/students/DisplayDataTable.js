77c7d7fbbe7060f9d1bc07013ef9647fa666830e
hftamayo <hftamayo@gmail.com>
2024-03-30 11:12:09 -0600

    last changes to deocuple header

--- src/components/entities/students/DisplayDataTable.js
+++ src/components/entities/students/DisplayDataTable.js
@@ -2,11 +2,12 @@ import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getStudents, deleteStudent } from "../../store/studentSlice";
 import { toast } from "react-toastify";
-import { FaSort, FaFilePdf, FaFileExcel } from "react-icons/fa";
+import { FaSort } from "react-icons/fa";
 
 import EntityRow from "./EntityRow";
 import dataTableClasses from "../../ui/crud/dataTableclasses";
 import NavButtons from "../../ui/crud/NavButtons";
+import CrudHeader from "../../ui/crud/CrudHeader";
 
 function DisplayDataTable() {
   const dispatch = useDispatch();
@@ -137,113 +138,19 @@ function DisplayDataTable() {
     );
   };
 
-  const handleSearch = (searchCriteria) => {
-    const lowerCaseSearchCriteria = searchCriteria.toLowerCase();
-
-    const filteredStudents = records.filter((student) =>
-      Object.values(student).some((value) =>
-        value.toString().toLowerCase().includes(lowerCaseSearchCriteria)
-      )
-    );
-
-    setCurrentPageRecords(
-      filteredStudents.slice(
-        (currentPage - 1) * recordsPerPage,
-        currentPage * recordsPerPage
-      )
-    );
-  };
-
-  const exportToExcel = () => {
-    // Convert the data to JSON
-    const jsonData = JSON.stringify(currentPageRecords, null, 2);
-    console.log("data to the PDF report: ", jsonData);
-
-    // Send the data to the backend
-    // fetch("/api/exportToPdf", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: jsonData,
-    // })
-    //   .then((response) => response.blob())
-    //   .then((blob) => {
-    //     // Create a link element and trigger a download
-    //     const url = window.URL.createObjectURL(blob);
-    //     const link = document.createElement("a");
-    //     link.href = url;
-    //     link.setAttribute("download", "students.pdf");
-    //     document.body.appendChild(link);
-    //     link.click();
-    //     link.remove();
-    //   })
-    //   .catch((error) => console.error("Error:", error));
-  };
-
-  const exportToPdf = () => {
-    const jsonData = JSON.stringify(currentPageRecords, null, 2);
-    console.log("data to the PDF report: ", jsonData);
-  };
-
   return (
     <div className="w-full flex flex-col min-h-[50vh] justify-center items-center">
       <h1 className="text-black text-3xl font-semibold font-Montserrat">
         Students Catalog
       </h1>
-      <div className="flex items-center justify-between p-4">
-        <div className="flex items-center">
-          <label
-            htmlFor="searchCriteria"
-            className="mr-2 block text-md font-medium text-gray-700"
-          >
-            Search Criteria:
-          </label>
-          <div className="border-gray-200 my-4 px-1"></div>
-          <input
-            type="text"
-            id="searchCriteria"
-            name="searchCriteria"
-            onChange={(event) => handleSearch(event.target.value)}
-            disabled={records.length === 0}
-            className="flex-grow text-md px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-500"
-          />
-        </div>
-        <div className="border-gray-100 my-4 px-24"></div>
-        <div className="flex-grow text-center">
-          Displaying:{" "}
-          <span className="font-semibold">
-            {currentPageRecords.length} of {records.length}
-          </span>{" records"}
-        </div>
-        <div className="border-gray-100 my-4 px-24"></div>
-
-        <div className="flex-shrink-0">
-          Records Per Page:{" "}
-          <select
-            className="px-3 py-1 bg-gray-800 text-white rounded-md"
-            value={recordsPerPage}
-            onChange={(event) => setRecordsPerPage(event.target.value)}
-            disabled={records.length === 0}
-          >
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="15">15</option>
-            <option value="-1">All</option>
-          </select>
-        </div>
-        <div className="border-gray-100 my-4 px-24"></div>
-
-        <div className="flex flex-grow">
-          Export to:{" "}
-          <button onClick={() => exportToPdf()} className="px-1">
-            <FaFilePdf size={24} color="red" />
-          </button>
-          <button onClick={() => exportToExcel()} className="px-1">
-            <FaFileExcel size={24} color="green" />
-          </button>
-        </div>
-      </div>
+      <CrudHeader
+        records={records}
+        currentPage={currentPage}
+        currentPageRecords={currentPageRecords}
+        setCurrentPageRecords={setCurrentPageRecords}
+        recordsPerPage={recordsPerPage}
+        setRecordsPerPage={setRecordsPerPage}
+      />
 
       <table className="w-[95%] text-center overflow-hidden overflow-y-scroll mt-8 border-2 border-b-2 border-black">
         <thead className="border-b bg-gray-800">
