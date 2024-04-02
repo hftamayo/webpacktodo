682f71a04b086b8410fe9710573a45b6ab37243f
hftamayo <hftamayo@gmail.com>
2024-04-02 08:46:35 -0600

    adding string messages for internationalization

---
+++ src/utils/StringMessages.js
@@ -0,0 +1,12 @@
+const stringMessages = {
+  confirmDialog: {
+    en: {
+      title: "Delete Selected Record",
+      message: "Are you sure you want to delete the record ",
+    },
+    es: {
+      title: "Eliminar registro seleccionado",
+      message: "¿Esta seguro que desea eliminar el registro seleccionado",
+    },
+  },
+};
