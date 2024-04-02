fc22c5706a00e24245411b868ebaa6a63863c3f1
hftamayo <hftamayo@gmail.com>
2024-04-02 09:16:43 -0600

    using env var for language app

--- src/components/store/dialogSlice.js
+++ src/components/store/dialogSlice.js
@@ -1,22 +1,35 @@
+import dotenv from "dotenv";
 import { createSlice } from "@reduxjs/toolkit";
 
 const dialogSlice = createSlice({
-    name: "dialog",
-    initialState: { isOpen: false, title: "", message: "" },
-    reducers: {
-        openDialog(state, action) {
-            state.isOpen = true;
-            state.title = action.payload.title;
-            state.message = action.payload.message;
-        },
-        closeDialog(state) {
-            state.isOpen = false;
-            state.title = "";
-            state.message = "";
-        },
+  name: "dialog",
+  initialState: {
+    isOpen: false,
+    title: "",
+    message: "",
+    activeLanguage: process.env.ACTIVE_LANGUAGE || "en",
+    entityName: "",
+  },
+  reducers: {
+    openDialog(state, action) {
+      state.isOpen = true;
+      state.title = action.payload.title;
+      state.message = action.payload.message;
+      state.entityName = action.payload.entityName;
     },
+    closeDialog(state) {
+      state.isOpen = false;
+      state.title = "";
+      state.message = "";
+      state.entityName = "";
+    },
+    setActiveLanguage(state, action) {
+      state.activeLanguage = action.payload;
+    },
+  },
 });
 
-export const { openDialog, closeDialog } = dialogSlice.actions;
+export const { openDialog, closeDialog, setActiveLanguage } =
+  dialogSlice.actions;
 
 export default dialogSlice.reducer;
