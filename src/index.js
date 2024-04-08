8230939f90138803285a2b4ec83e9a14744fce2e
Herbert Tamayo <hftamayo@gmail.com>
2024-04-08 08:11:13 -0600

    loading enviro file from the beginning

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./components/store/store.js";
 
+require('dotenv').config();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
