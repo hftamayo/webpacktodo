a279211e87453a31a4b97fda0defa5643e3e125b
Herbert Tamayo <hftamayo@gmail.com>
2024-04-08 09:23:38 -0600

    integrating delete business logic on dialogSlicer

--- src/components/store/dialogSlice.js
+++ src/components/store/dialogSlice.js
@@ -1,5 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { deleteStudent, getStudents } from "./studentsSlice";
 import StringMessages from "../../utils/StringMessages";
+import { toast } from "react-toastify";
+
+const deleteEntity = createAsyncThunk(
+  "dialog/deleteEntity",
+  async (id, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await deleteStudent(id);
+      dispatch(getStudents());
+      return response;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 const dialogSlice = createSlice({
   name: "dialog",
@@ -10,9 +25,10 @@ const dialogSlice = createSlice({
     message: "",
     entityId: null,
     entityName: "",
+    errorMessage: null,
   },
   reducers: {
-    openDialog(state, action) {
+    openDialog: (state, action) => {
       state.isOpen = true;
       state.entityId = action.payload.id;
       state.entityName = action.payload.entityName;
@@ -21,20 +37,48 @@ const dialogSlice = createSlice({
       state.title = title;
       state.message = message;
     },
-    closeDialog(state) {
+    closeDialog: (state) => {
       state.isOpen = false;
       state.title = "";
       state.message = "";
       state.entityId = null;
       state.entityName = "";
     },
-    setActiveLanguage(state, action) {
+    setActiveLanguage: (state, action) => {
       state.activeLanguage = action.payload;
       const { title, message } =
         StringMessages.confirmDialog[state.activeLanguage];
       state.title = title;
       state.message = message;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(deleteEntity.pending, (state) => {
+        state.isOpen = true;
+      })
+      .addCase(deleteEntity.fulfilled, (state) => {
+        state.isOpen = false;
+        state.title = "";
+        state.message = "";
+        state.entityId = null;
+        state.entityName = "";
+        toast.success("Data deleted permanently", {
+          className: "bg-black text-yellow-500",
+          progressClassName: "bg-blue-600",
+        });
+      })
+      .addCase(deleteEntity.rejected, (state) => {
+        state.isOpen = false;
+        state.title = "";
+        state.message = "";
+        state.entityId = null;
+        state.entityName = "";
+        state.errorMessage = action.error.message;
+        toast.error(
+          "An error occurred while trying to delete the selected data, the event was reported. Please try again later. "
+        );
+      });
   },
 });
 
