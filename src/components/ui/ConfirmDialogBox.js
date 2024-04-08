a84f0e31d8474a22394d2849143696323e7ce446
Herbert Tamayo <hftamayo@gmail.com>
2024-04-08 09:52:29 -0600

    defining dispatch object and last touches to trigger the delete workflow

--- src/components/ui/ConfirmDialogBox.js
+++ src/components/ui/ConfirmDialogBox.js
@@ -1,10 +1,16 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteEntity } from "../../store/dialogSlice";
 import DialogBox from "./DialogBox";
 
-function ConfirmDialogBox({ onDelete }) {
-  const { isOpen, title, message, entityName } = useSelector(
+function ConfirmDialogBox() {
+  const dispatch = useDispatch();
+  const { isOpen, title, message, entityId, entityName } = useSelector(
     (state) => state.dialog
   );
+
+  const handleDeleteEntity = (id) => {
+    dispatch(deleteEntity(id));
+  };
 
   if (!isOpen) return null;
 
@@ -21,7 +27,7 @@ function ConfirmDialogBox({ onDelete }) {
         <div className="flex gap-4 mt-6 justify-center">
           <button
             className="px-6 py-2 font-normal text-white bg-red-600 rounded-lg"
-            onClick={onDelete}
+            onClick={() => handleDeleteEntity(entityId)}
           >
             Delete
           </button>
