ee565b1cd8a7d5d7c4ef3fa1535e02bad3a15e34
Herbert Tamayo <hftamayo@gmail.com>
2024-04-09 11:03:50 -0600

    adding os and crypto deps to fallback

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,8 @@ module.exports = {
   resolve: {
     fallback: {
       path: require.resolve("path-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      crypto: require.resolve("crypto-browserify"),
     },
   },
   devServer: {
