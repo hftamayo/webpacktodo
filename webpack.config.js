3b24bbf918f503fa74fc1e73da5fc71bffc0a309
Herbert Tamayo <hftamayo@gmail.com>
2024-04-09 11:49:17 -0600

    adding dotenv-webpack as plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -20,6 +21,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [new Dotenv()],
   devServer: {
     contentBase: "./dist",
     port: 3000,
