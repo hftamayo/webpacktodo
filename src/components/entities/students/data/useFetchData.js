012c9aa4f16c84f0e33a97776b8d56da67fc006a
Herbert Tamayo <hftamayo@gmail.com>
2024-04-11 09:33:08 -0600

    hook for data loading

---
+++ src/components/entities/students/data/useFetchData.js
@@ -0,0 +1,26 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { getStudents } from "../../../store/studentSlice";
+import { toast } from 'react-toastify';
+
+export default function useFetchData() {
+  const dispatch = useDispatch();
+
+  const loadData = useCallback(() => {
+    try {
+      dispatch(getStudents()).then((action) => {
+        if (getStudents.fulfilled.match(action)) {
+          console.log("Status: succeeded"); // Log the status
+        } else if (getStudents.rejected.match(action)) {
+          console.log("Status: failed"); // Log the status
+        }
+      });
+    } catch (error) {
+      toast.error(
+        "An error occurred while trying to load the data, the event was reported. Please try again later."
+      );
+    }
+  }, [dispatch]);
+
+  return loadData;
+}
