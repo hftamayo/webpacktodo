2f3b066833ca41d45c9dcc23e3950223b380b6b7
Herbert Tamayo <hftamayo@gmail.com>
2024-04-15 09:47:11 -0600

    simplifying the business logic of pagination

--- src/components/ui/crud/usePagination.js
+++ src/components/ui/crud/usePagination.js
@@ -5,27 +5,14 @@ export default function usePagination(records, currentPage, recordsPerPage) {
   const [numberOfPages, setNumberOfPages] = useState(0);
 
   useEffect(() => {
-    let pages;
-    let currentPageRecords;
-
     const indexOfLastRecord = currentPage * recordsPerPage;
     const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
     const currentRecords = records
       ? records.slice(indexOfFirstRecord, indexOfLastRecord)
       : [];
 
-    if (recordsPerPage === -1) {
-      pages = 1;
-      currentPageRecords = currentRecords;
-    } else {
-      pages = Math.ceil(currentRecords.length / recordsPerPage);
-      currentPageRecords = currentRecords.slice(
-        (currentPage - 1) * recordsPerPage,
-        currentPage * recordsPerPage
-      );
-    }
-    setNumberOfPages(pages);
-    setCurrentPageRecords(pages === 0 ? [] : currentPageRecords);
+      setNumberOfPages(Math.ceil(records.length / recordsPerPage));
+      setCurrentPageRecords(currentRecords);
   }, [records, currentPage, recordsPerPage]);
 
   return {
