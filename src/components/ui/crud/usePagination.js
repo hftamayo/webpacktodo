0831643aad26908d2d32549866805ecb64805f16
Herbert Tamayo <hftamayo@gmail.com>
2024-04-16 14:58:39 -0600

    infinite loop using separated sort and pagination

--- src/components/ui/crud/usePagination.js
+++ src/components/ui/crud/usePagination.js
@@ -1,27 +1,14 @@
 import { useState, useEffect } from "react";
 
 export default function usePagination(records, currentPage, recordsPerPage) {
-  const [currentPageRecords, setCurrentPageRecords] = useState([]);
-  const [numberOfPages, setNumberOfPages] = useState(0);
+  const [paginatedRecords, setPaginatedPageRecords] = useState([]);
+  //const [numberOfPages, setNumberOfPages] = useState(0);
 
   useEffect(() => {
-    if(Array.isArray(records)){
-    const indexOfLastRecord = currentPage * recordsPerPage;
-    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
-    const currentRecords = records
-      ? records.slice(indexOfFirstRecord, indexOfLastRecord)
-      : [];
-
-      setNumberOfPages(Math.ceil(records.length / recordsPerPage));
-      setCurrentPageRecords(currentRecords);
-    } else {
-      setNumberOfPages(0);
-      setCurrentPageRecords([]);
-    }
+    const startIndex = (currentPage - 1) * recordsPerPage;
+    const endIndex = startIndex + recordsPerPage;
+    setPaginatedPageRecords(records.slice(startIndex, endIndex));
   }, [records, currentPage, recordsPerPage]);
 
-  return {
-    paginatedNumberOfPages: numberOfPages,
-    paginatedRecords: currentPageRecords,
-  };
+  return { paginatedRecords };
 }
