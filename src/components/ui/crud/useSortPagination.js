14e182245515be17ddeb63205a697f6c321c3fce
Herbert Tamayo <hftamayo@gmail.com>
2024-04-17 09:41:08 -0600

    sorting by default the active records

--- src/components/ui/crud/useSortPagination.js
+++ src/components/ui/crud/useSortPagination.js
@@ -3,7 +3,8 @@ import { useEffect, useRef, useState, useCallback } from "react";
 export default function useSortPagination(
   records,
   currentPage,
-  recordsPerPage
+  recordsPerPage,
+  searchCriteria
 ) {
   const [sortField, setSortField] = useState("id");
   const [sortDirection, setSortDirection] = useState("desc");
@@ -17,32 +18,36 @@ export default function useSortPagination(
 
   const handleSort = useCallback(
     (field = "id") => {
-      if (prevRecordsRef.current !== records) {
-        let direction = "desc";
-        if (field === sortField) {
-          direction = sortDirection === "asc" ? "desc" : "asc";
-        }
-        setSortField(field);
-        setSortDirection(direction);
-
-        const sorted = [...records].sort((a, b) => {
-          if (a[field] < b[field]) {
-            return direction === "asc" ? -1 : 1;
-          }
-          if (a[field] > b[field]) {
-            return direction === "asc" ? 1 : -1;
-          }
-          return 0;
-        });
-        setSortedRecords(sorted);
+      let direction = "desc";
+      if (field === sortField) {
+        direction = sortDirection === "asc" ? "desc" : "asc";
       }
+      setSortField(field);
+      setSortDirection(direction);
+
+      const filteredRecords = records.filter((record) =>
+        Object.values(record).some((value) =>
+          value.toString().toLowerCase().includes(searchCriteria.toLowerCase())
+        )
+      );
+
+      const sorted = [...filteredRecords].sort((a, b) => {
+        if (a[field] < b[field]) {
+          return direction === "asc" ? -1 : 1;
+        }
+        if (a[field] > b[field]) {
+          return direction === "asc" ? 1 : -1;
+        }
+        return 0;
+      });
+      setSortedRecords(sorted);
     },
-    [records, sortField, sortDirection]
+    [records, sortField, sortDirection, searchCriteria]
   );
 
   useEffect(() => {
     handleSort(sortField);
-  }, [records, sortField]);
+  }, [records, sortField, searchCriteria]);
 
   useEffect(() => {
     const startIndex = (currentPage - 1) * recordsPerPage;
