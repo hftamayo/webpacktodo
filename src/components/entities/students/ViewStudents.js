212ddaa9817a9c628a07340fc2db4ae4a3f70564
Herbert Tamayo <hftamayo@gmail.com>
2024-04-19 08:56:49 -0600

    extracting the new button to header component

--- src/components/entities/students/ViewStudents.js
+++ src/components/entities/students/ViewStudents.js
@@ -1,21 +1,10 @@
 import React from "react";
 import DataTable from "./data/DataTable";
-import { Link } from "react-router-dom";
-import { FaPlus } from "react-icons/fa";
+
 
 function ViewStudents() {
   return (
     <div className="w-full h-full flex flex-col px-10 py-8 justify-center items-start">
-      <div>
-        <Link
-          to={`/newstudent`}
-          className="flex items-center px-6 py-2 font-normal text-white bg-green-600 rounded-lg w-30"
-        >
-          <FaPlus />
-          <span className="ml-2">New</span>
-        </Link>
-      </div>
-
       <DataTable />
     </div>
   );
